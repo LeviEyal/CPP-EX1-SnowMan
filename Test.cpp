2c81202c855bfc3ce29a795bcfe532fee47eabd1
Your Name <happyeyal@gmail.com>
2021-03-08 10:24:57 +0200

    added tests

--- Test.cpp
+++ Test.cpp
@@ -5,23 +5,54 @@
 
 using namespace ariel;
 
-TEST_CASE("testing input number"){
-    CHECK_THROWS(snowman(283));
-    CHECK_THROWS(snowman(0));
-    CHECK_THROWS(snowman(111144112));
-    CHECK_THROWS(snowman(10114711));
+TEST_CASE("number digits out of range"){
+    CHECK_THROWS(snowman(18273572));
+    CHECK_THROWS(snowman(11111119));
+    CHECK_THROWS(snowman(11111110));
+    CHECK_THROWS(snowman(12532111));
+    CHECK_THROWS(snowman(29211234));
+}
+
+TEST_CASE("Input number too small"){
+    CHECK_THROWS(snowman(2321435));
+    CHECK_THROWS(snowman(2314321));
+    CHECK_THROWS(snowman(1234));
+    CHECK_THROWS(snowman(1));
+}
+
+TEST_CASE("Input number too big"){
+    CHECK_THROWS(snowman(232142735));
+    CHECK_THROWS(snowman(111111111));
+    CHECK_THROWS(snowman(123412341));
+    CHECK_THROWS(snowman(444444444));
 }
 
 TEST_CASE("Good snowman code") {
     CHECK(snowman(11114411) == string("       \n _===_ \n (.,.) \n ( : ) \n ( : ) "));
     CHECK(snowman(33232124) == string("   _   \n  /_\\  \n\\(o_O) \n (] [)>\n (   ) "));
-    CHECK(snowman(33232124) == string("   _   \n  /_\\  \n\\(o_O) \n (] [)>\n (   ) "));
+    CHECK(snowman(12222212) == string("       \n _===_ \n\\(o.o)/\n ( : ) \n (\" \") "));
+    CHECK(snowman(32443333) == string("   _   \n  /_\\  \n (-.-) \n/(> <)\\\n (___) "));
+    CHECK(snowman(44444432) == string("  ___  \n (_*_) \n (- -) \n (> <) \n (\" \") "));
+    CHECK(snowman(44242123) == string("  ___  \n (_*_) \n\\(o -) \n (] [)>\n (___) "));
+    CHECK(snowman(41341144) == string("  ___  \n (_*_) \n (O,-) \n<(   )>\n (   ) "));
+    CHECK(snowman(11111111) == string("       \n _===_ \n (.,.) \n<( : )>\n ( : ) "));
+    CHECK(snowman(22222222) == string("  ___  \n ..... \n\\(o.o)/\n (] [) \n (\" \") "));
+    CHECK(snowman(33333333) == string("   _   \n  /_\\  \n (O_O) \n/(> <)\\\n (___) "));
+    CHECK(snowman(44444444) == string("  ___  \n (_*_) \n (- -) \n (   ) \n (   ) "));
 }
 
 TEST_CASE("bad snowman code") {
-    // cout << string("_===_\n(.,.)\n( : )\n( : )") << endl;
-    // CHECK(snowman(11114411) == string("_===_\n(.,.)\n( : )\n( : )"));
-    /* Add more checks here */
+    CHECK(snowman(11114411) != string(" _===_ \n (.,.) \n ( : ) \n ( : ) "));
+    CHECK(snowman(33232124) != string("   _   \n  /_\\  \n\\(0_O) \n (] [)>\n (   ) "));
+    CHECK(snowman(12222212) != string("       \n _===_\n\\(o.o)/\n ( : ) \n (\" \") "));
+    CHECK(snowman(32443333) != string("   _   \n  /_\\  \n (-.-) \n/(> <)\\ \n (___) "));
+    CHECK(snowman(44444432) != string("  ___  \n (_*_) \n (- -) \n (> <) \n ( \") "));
+    CHECK(snowman(44242123) != string("  ___ \n (_*_) \n\\(o -) \n (] [)>\n (___) "));
+    CHECK(snowman(41341144) != string("  ___  \n (_*_) \n (O, -) \n<(  )>\n (   ) "));
+    CHECK(snowman(11111111) != string("      \n _===_ \n (.,.) \n<( : )>\n ( : ) "));
+    CHECK(snowman(22222222) != string("  ___  \n .... \n\\(o.o)/\n (] [) \n (\" \") "));
+    CHECK(snowman(33333333) != string("   _   \n  /_\\  \n (O_O) \n/(> <)\\\n (___)"));
+    CHECK(snowman(44444444) != string("  ___  \n (_*_) \n (- -) \n (  ) \n (   ) "));
 }
 
 
